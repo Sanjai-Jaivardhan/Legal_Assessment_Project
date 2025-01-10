4db877060ddbb41a5a4ba6463b015c3d0726cb08
Sanjai-Jaivardhan <sanjai.jaivardhan@gmail.com>
2025-01-10 20:22:52 +0530

    providers for http
    updated the http client according to angular by changing the providers in config filw

--- Law_Software/src/app/scenario-description/scenario-description.component.ts
+++ Law_Software/src/app/scenario-description/scenario-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatButtonModule} from '@angular/material/button';
+import { timestamp } from 'rxjs';
 @Component({
   selector: 'app-scenario-description',
   standalone: true,
@@ -12,4 +13,13 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class ScenarioDescriptionComponent {
   panelOpenState = false;
+  title='An unexpeccted chaos'
+  onButtonClick(event: Event): void {
+    const buttonId = (event.currentTarget as HTMLElement).id;
+    const eventTime = Date.now() - performance.now() + event.timeStamp;
+    console.log('Button ID:', buttonId);
+    console.log("Event Type:",new Date(event.timeStamp))
+    console.log('Correct Event Time:', new Date(eventTime).toString());
+  }
+  
 }
